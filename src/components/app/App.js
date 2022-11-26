8f3ef07ce6b3e795310fb7ef924b8a6572a7c96b
Viktor Pekin <Viktorpekin43@gmail.com>
2022-11-26 19:09:05 +0700

    Add: create-react-add and file structure

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import Header from '../header/Header'
+import Footer from '../footer/Footer'
+import Main from '../main/Main'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
