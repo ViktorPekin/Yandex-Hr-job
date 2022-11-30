ee98c65b35d8bedb66ca1b68a15671fddf72560b
Boris Gaidukevich <Borishahn@yandex.ru>
2022-11-30 03:22:42 +0300

    добавлен адаптив блок 3 и блок

--- src/components/main/howToBecome/howToBecome.js
+++ src/components/main/howToBecome/howToBecome.js
@@ -8,43 +8,57 @@ const HowToBecome = () => {
 
   const headerAnimation = inView ? 'howToBecome__header howToBecome__header_animation' : 'howToBecome__header'
   const firstTaskAnimation = inView
-    ? 'howToBecome__firstTask howToBecome__firstTask_animation'
-    : 'howToBecome__firstTask'
+    ? 'howToBecome__task howToBecome__task_first firstTask_animation'
+    : 'howToBecome__task howToBecome__task_first'
   const secondTaskAnimation = inView
-    ? 'howToBecome__secondTask howToBecome__secondTask_animation'
-    : 'howToBecome__secondTask'
+    ? 'howToBecome__task howToBecome__task_second secondTask_animation'
+    : 'howToBecome__task howToBecome__task_second'
   const thirdTaskAnimation = inView
-    ? 'howToBecome__thirdTask howToBecome__thirdTask_animation'
-    : 'howToBecome__thirdTask'
+    ? 'howToBecome__task howToBecome__task_third thirdTask_animation'
+    : 'howToBecome__task howToBecome__task_third'
+  const buttonAnimation = inView ? 'howToBecome__button button_animation' : 'howToBecome__button'
+
+  const scrollTo = () => {
+    const element = document.querySelector('#vacancies')
+    element.scrollIntoView()
+  }
+
   return (
     <section className='howToBecome' ref={ref}>
       <h2 className={headerAnimation}>Как стать экспертом?</h2>
-      <ul className='howToBecome__list'>
-        <li className={firstTaskAnimation}>
-          <span className='howToBecome__counter'>1</span>
-          <p className='howToBecome__context'>
-            <span>Выберите роль</span>
-            <span>и заполните заявку</span>
-          </p>
-        </li>
-        <li className={secondTaskAnimation}>
-          <span className='howToBecome__counter'>2</span>
-          <p className='howToBecome__context'>
-            <span>Пройдите отбор</span>
-            <span>и бесплатное обучение</span>
-          </p>
-        </li>
-        <li className={thirdTaskAnimation}>
-          <span className='howToBecome__counter'>3</span>
-          <p className='howToBecome__context'>
-            <span>Начните делиться знаниями</span>
-            <span>со студентами Яндекс Практикума</span>
-          </p>
-        </li>
-      </ul>
-      <button type='button' className='howToBecome__button'>
-        Начать делиться знаниями
-      </button>
+      <div className='howToBecome__list-wrapper'>
+        <ul className='howToBecome__list'>
+          <li className={firstTaskAnimation}>
+            <span className='howToBecome__counter'>1</span>
+            <p className='howToBecome__context'>
+              <span>Выберите роль</span>
+              <span>и заполните заявку</span>
+            </p>
+          </li>
+          <li className={secondTaskAnimation}>
+            <span className='howToBecome__counter'>2</span>
+            <p className='howToBecome__context'>
+              <span>Пройдите отбор</span>
+              <span>и бесплатное обучение</span>
+            </p>
+          </li>
+          <li className={thirdTaskAnimation}>
+            <span className='howToBecome__counter'>3</span>
+            <p className='howToBecome__context'>
+              <span>
+                Делитесь знаниями <br />
+                со студентами
+              </span>
+            </p>
+          </li>
+        </ul>
+        <button type='button' className={buttonAnimation} onClick={() => scrollTo()}>
+          <span className='howToBecome__button_connect'>
+            Присоединиться <br />к команде
+          </span>
+          <span className='howToBecome__button_form'>Заполнить форму</span>
+        </button>
+      </div>
     </section>
   )
 }
