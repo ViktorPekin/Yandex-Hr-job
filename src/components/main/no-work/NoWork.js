2fc8d3f6de9a16b4f9c1fd18e2866ad8cb5205e6
Viktor Pekin <Viktorpekin43@gmail.com>
2022-11-30 15:51:42 +0700

    finish: component no-work

--- src/components/main/no-work/NoWork.js
+++ src/components/main/no-work/NoWork.js
@@ -1,22 +1,16 @@
-import { useState } from 'react';
-
-import NoworkPopup from '../nowork-popup/NoworkPopup';
-import './no-work.css';
+import './no-work.css'
 
 const NoWork = () => {
-
-  const [ popupOpen, setPopupOpen] = useState(false);
-
-  const changePopup = (bool) => {
-    setPopupOpen(bool)
-  }
-
-  return <section className='no-work font_display'>
-    <h2 className='no-work__title'>Нет подходящей вакансии?</h2>
-    <p className='no-work__text'>Расскажите нам о своем опыте, а мы подберем варианты</p>
-    <button type='button' className='no-work__button' onClick={()=>changePopup(true)}>Начать делиться знаниями</button>
-    <NoworkPopup popupOpen={popupOpen} popupClose={changePopup}/>
-  </section>
+  return (
+    <section className='no-work font_display'>
+      <h2 className='no-work__title font_display'>Нет подходящего предложения?</h2>
+      <p className='no-work__text'>Расскажите нам о своем опыте, а мы попробуем подобрать варианты</p>
+      <button type='button' className='no-work__button'>
+        Заполнить форму
+      </button>
+      <div className='no-work__img'></div>
+    </section>
+  )
 }
 
 export default NoWork
