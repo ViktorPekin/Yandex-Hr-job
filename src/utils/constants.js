32314a69f6494b8d531181f07d9d7db8619fb400
Nikolay Dolgov <koly201020@yandex.ru>
2022-12-03 19:02:07 +0300

    Empty commit

--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const SCREEN_PC = 1024
-export const NUMBER_CARD_PC = 12
-export const NUMBER_CARD_MOBILE = 6
-
-export const PROGRAMMING = 0
-export const ANALYTICS = 1
-export const DESIGN = 2
-export const MARKETING = 3
-export const MANAGEMENT = 4
